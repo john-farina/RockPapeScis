45047c180364ec37890efa35bc38ceb4f011c20a
onefootfarina <clichecontent@gmail.com>
2022-02-25 13:35:49 -0500

    Added some code to make it functional

--- rockPaperCode.js
+++ rockPaperCode.js
@@ -1,8 +1,8 @@
 function computerPlay(){
-    let random = (Math.random()*7)+1;
-    if (random<=2){
+    let random = (Math.random()*9)+1;
+    if (random<=3){
         return('rock');
-    } else if (random>3 && random<=4){
+    } else if (random>=4 && random<=7){
         return('paper');
     } else {
         return('scissors');
@@ -10,11 +10,13 @@ function computerPlay(){
 }
 
 let score = 0;
+let computerScore = 0;
     function playRound(playerSelection,computerSelection){
     if (playerSelection == 'rock') {
         if (computerSelection == 'rock') {
             return(`TIE! Rock Doesn't Beat Rock.`);
         } else if (computerSelection == 'paper') {
+            computerScore++;
             return(`YOU LOSE! Paper Beats Rock.`);
         } else if (computerSelection == 'scissors') {
             score++;
@@ -28,10 +30,12 @@ let score = 0;
         } else if (computerSelection == 'paper') {
             return(`TIE! Paper Doesn't Beat Paper.`)
         } else if (computerSelection == 'scissors') {
+            computerScore++;
             return('YOU LOSE! Scissors Beats Paper.')
         }
     }   else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
+            computerScore++;
             return('YOU LOSE! Rock Beats Scissors.')
         } else if (computerSelection == 'paper') {
             score++;
@@ -42,22 +46,46 @@ let score = 0;
     }
 }
 
-let round = 1;
+const displayDiv = document.querySelector('#displayDiv');
+displayDiv.style.backgroundColor="cyan";
+displayDiv.style.textAlign='center';
 
-while(round <= 5){
-    let playerPrompt = prompt('Rock, Paper, or Scissors');
-    let playerSelection = playerPrompt.toLowerCase();
-    let computerSelection = computerPlay();
-    console.log(`ROUND ${round}.`,playRound(playerSelection,computerSelection), `Your answer was ${playerSelection}!`);
-    round++;
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', () => {
+let playerSelection= 'rock';
+let computerSelection = computerPlay();
+displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+});
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', () => {
+let playerSelection= 'paper';
+let computerSelection = computerPlay();
+displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+});
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', () => {
+let playerSelection = 'scissors';
+let computerSelection = computerPlay();
+displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+});
+// let round = 1;
+
+// while(round <= 5){
+//     let playerPrompt = prompt('Rock, Paper, or Scissors');
+//     let playerSelection = playerPrompt.toLowerCase();
+//     let computerSelection = computerPlay();
+//     console.log(`ROUND ${round}.`,playRound(playerSelection,computerSelection), `Your answer was ${playerSelection}!`);
+//     round++;
     
-    if (round>5) {
-        console.log(`You Got ${score/5*100}%!`);
-        if (score<3) {
-            console.log(`You Lost! Loser!`);
-        } else {
-            console.log(`Hey You Won! must be hacks`);
-        }
-    }
-}  
+//     if (round>5) {
+//         console.log(`You Got ${score/5*100}%!`);
+//         if (score<3) {
+//             console.log(`You Lost! Loser!`);
+//         } else {
+//             console.log(`Hey You Won! must be hacks`);
+//         }
+//     }
+// }  
 
