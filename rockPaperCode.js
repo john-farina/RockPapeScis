883043207be550f62bdac7c2024a6e9d6faa589b
onefootfarina <clichecontent@gmail.com>
2022-02-25 20:35:40 -0500

    added scores and stuff, cant figure out rounds tho

--- rockPaperCode.js
+++ rockPaperCode.js
@@ -9,6 +9,7 @@ function computerPlay(){
     }
 }
 
+//ROCK PAPER SCISSORS FUNCTION
 let score = 0;
 let computerScore = 0;
     function playRound(playerSelection,computerSelection){
@@ -46,30 +47,58 @@ let computerScore = 0;
     }
 }
 
+//DIV WHERE IT SAYS YOU WON OR NOT
 const displayDiv = document.querySelector('#displayDiv');
 displayDiv.style.backgroundColor="cyan";
 displayDiv.style.textAlign='center';
 
+//COMPUTER SCORE BOX
+const compScore = document.querySelector('#comp-score');
+compScore.style.color='red';
+
+//PLAYER SCORE BOX
+const playScore = document.querySelector('#player-score');
+playScore.style.color='red';
+
+const finalMessage = document.querySelector('#final-message');
+let round = 1;
+finalMessage.textContent=`${round}`;
+
+//ROCK BUTTON
 const rockBtn = document.querySelector('#rockBtn');
 rockBtn.addEventListener('click', () => {
 let playerSelection= 'rock';
 let computerSelection = computerPlay();
-displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+displayDiv.textContent=(playRound(playerSelection,computerSelection));
+compScore.textContent=`${computerScore}`;
+playScore.textContent=`${score}`;
 });
 
+//PAPER BUTTON
 const paperBtn = document.querySelector('#paperBtn');
 paperBtn.addEventListener('click', () => {
 let playerSelection= 'paper';
 let computerSelection = computerPlay();
-displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+displayDiv.textContent=(playRound(playerSelection,computerSelection));
+compScore.textContent=`${computerScore}`;
+playScore.textContent=`${score}`;
 });
 
+//SCISSORS BUTTON
 const scissorsBtn = document.querySelector('#scissorsBtn');
 scissorsBtn.addEventListener('click', () => {
 let playerSelection = 'scissors';
 let computerSelection = computerPlay();
-displayDiv.textContent=(playRound(playerSelection,computerSelection)+`Your score is ${score} vs The computers ${computerScore}`);
+displayDiv.textContent=(playRound(playerSelection,computerSelection));
+compScore.textContent=`${computerScore}`;
+playScore.textContent=`${score}`;
 });
+
+
+if (compScore<playScore){
+    finalMessage.textContent='nice ur winning';
+}
+
 // let round = 1;
 
 // while(round <= 5){
